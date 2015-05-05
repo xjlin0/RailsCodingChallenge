ed51aaec4faffc58372be9b15f967b123244179d
Tim Cull <tim.cull@touchofmodern.com>
2015-05-05 10:39:58 -0700

    first version of coding challenge

---
+++ lib/cuboid.rb
@@ -0,0 +1,17 @@
+
+class Cuboid
+  
+  #BEGIN public methods that should be your starting point
+
+  def move_to!(x, y, z)
+  end
+  
+  def vertices
+  end
+  
+  #returns true if the two cuboids intersect each other.  False otherwise.
+  def intersects?(other)
+  end
+
+  #END public methods that should be your starting point  
+end
