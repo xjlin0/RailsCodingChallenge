d130c66fbcce5fac35a74604855392745bdfcaa0
Jack <xjlin0@gmail.com>
2015-07-16 12:10:19 -0700

    rotation finished

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -25,38 +25,33 @@ class Point
     return Point.new(x: @x, y: @y, z: @z + shift)
   end
 
-  def above_wall_of?(other) #check if a point is within wall limits of the other point
-    other_point = other.is_a?(Point) ? other : raise("not a Point!")
-    return false if @x < other_point.x
-    return false if @y < other_point.y
-    return false if @z < other_point.z
-    return true
-  end
 end
 
 
 class Cuboid
   attr_accessor :origin, :length, :width, :height
   #BEGIN public methods that should be your starting point
-    def initialize(options={})  # length is on x axle, width is on y axle, height is on z axle
+
+    # length is on x axle, width is on y axle, height is on z axle
+    def initialize(options={})
       origin = options.fetch(:origin) rescue raise("need a Point as the origin")
       length = options.fetch(:length) rescue raise("need length")
       width  = options.fetch(:width)  rescue raise("need width")
       height = options.fetch(:height) rescue raise("need height")
       @origin = origin.is_a?(Point) ? origin : raise("not a Point!")
       @length = options.fetch(:length).is_a?(Numeric) && length > 0 ? length : raise("need positive number!")
-      @width  = options.fetch(:width).is_a?(Numeric) && width  > 0 ? width  : raise("need positive number!")
+      @width  = options.fetch(:width).is_a?(Numeric)  && width  > 0 ? width  : raise("need positive number!")
       @height = options.fetch(:height).is_a?(Numeric) && height > 0 ? height : raise("need positive number!")
     end
 
+  # Inputs of move_to! method is order-dependent, should be in the sequence of x , y , z.
   def move_to!(x, y, z)
-    # Inputs of move_to! method is order-dependent, should be in the sequence of x , y , z.
     new_origin = Point.new(x: x, y: y, z: z)
     @origin = new_origin
   end
 
+  # outout 8 points of a cuboid [length on x, width on y, height on z]
   def vertices
-    # outout 8 points of a cuboid [length on x, width on y, height on z]
     o  = @origin
     ox = @origin.movex(@length)
     oy = @origin.movey(@width)
@@ -68,7 +63,8 @@ class Cuboid
     return [ o, ox, oy, oxy, oz, oxz, oyz, oxyz ]
   end
 
-  def contains?(point) #Test if a point is excusively inside of a cuboid
+  #Test if a point is excusively inside of a cuboid
+  def contains?(point)
     p = point.is_a?(Point) ? point : raise("not a Point!")
     return false if p.x < @origin.x || p.x > @origin.movex(@length).x
     return false if p.y < @origin.y || p.y > @origin.movey(@width).y
@@ -83,6 +79,18 @@ class Cuboid
       return true if self.contains?(vertice)
     end
     return false
+  end
+
+  def rotate_at_x
+    @height, @width = @width, @height
+  end
+
+  def rotate_at_y
+    @height, @length = @length, @height
+  end
+
+  def rotate_at_z
+    @length, @width = @width, @length
   end
 
   #END public methods that should be your starting point
